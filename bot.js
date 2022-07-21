07ceb77bad31a95adcf53c1cb974f27a08dedf01
deepak260901 <105791403+deepak260901@users.noreply.github.com>
2022-07-21 10:47:09 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,19 +33,19 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.wame = ['27634090203'] 
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
-global.pengguna = 'Drips' //username
-global.footer = 'ZIM BOT INC'
-global.botnma = 'ZIM BOT INC' //bot name
-global.botname = 'ZIM BOT V4'
-global.ownernma = 'Drips' //owner name
-global.packname = 'Drips' //sticker package name
-global.author = 'By Drips Memes' //sticker author name
+global.owner = ['919015286428','15593547291'] //owner number +27634090203
+global.wame = ['919015286428','15593547291'] 
+global.pemilik = ['919015286428','15593547291'] //owner number +27634090203
+global.premium = ['919015286428','15593547291'] //premium number +27634090203
+global.pengguna = 'Deepak' //username
+global.footer = 'DEEPAK BOT INC'
+global.botnma = 'GAPPU BOT INC' //bot name
+global.botname = 'GAPPU BOT'
+global.ownernma = 'Deepak' //owner name
+global.packname = 'GAPPU-BOT🔥💫' //sticker package name
+global.author = 'DEEPAK-BOT♥️✨' //sticker author name
 global.sessionName = 'session' //session name
-global.prefa = ['#','!','/',''] //prefix
+global.prefa = ['#'] //prefix
 global.sp = '🔵' //sp
 global.dripstyping = false
 global.dripsreadgroup = false
@@ -55,7 +55,7 @@ global.available = false
 global.unavailable = true
 global.limittot = 100,
 global.thumb = fs.readFileSync('./Zimbot/drips.jpg')
-global.reactmoji = '🇿🇼'
+global.reactmoji = '😼'
 global.imgjoinrpg = fs.readFileSync('./image/joinrpg.jpg')
 global.imgmining = fs.readFileSync('./image/mining.jpg')
 global.imgdevil = fs.readFileSync('./image/img/devil.jpg')
@@ -72,15 +72,15 @@ global.f5 = 'application/pdf'
 global.f6 = 'application/vnd.android.package-archive'
 global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text='
 global.mess = {
-    success: 'Takaenda Done!',
+    success: 'Takaenda Done😸✅!',
     admin: '*This feature is only for admin!*',
-    botAdmin: '*zim bot  must be admin first!*',
-    owner: '*this feature is only for zimbot owner*',
-    group: '*Feature is only for groups!*',
-    grouponly: '*Group Mode Only*',
-    private: 'Features Used Only For Private Chat!',
-    bot: 'This feature in only for the bot number',
-    wait: '*whoa wait a moment zim bot proccessing*',
+    botAdmin: '*Gappu bot 😉 must be admin first!*',
+    owner: '*this feature is only for gappubot 😁 owner*',
+    group: '*Feature is only 😌 for groups!*',
+    grouponly: '*Group ☺️ Mode Only*',
+    private: 'Features Used Only For Private 😜 Chat!',
+    bot: 'This feature in only for the 😅 bot number',
+    wait: '*whoa wait a moment gappu bot 😤 proccessing*',
     endLimit: '*Your daily limit has expired the limit will be reseted every 12 hours*',
     noPetualang: '*you are not in rpg database type #joinrpg* _ur name_'
 }
