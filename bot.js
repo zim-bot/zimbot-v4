446b3eb8f92a5a37168babe60efd7d85cee3f06e
10Chanuka <93696963+10Chanuka@users.noreply.github.com>
2022-08-09 19:46:59 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,17 +33,17 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.wame = ['27634090203'] 
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
-global.pengguna = 'Drips' //username
+global.owner = ['94714841631',''] //owner number +27634090203
+global.wame = ['94714841631'] 
+global.pemilik = ['94714841631'] //owner number +27634090203
+global.premium = ['94714841631'] //premium number +27634090203
+global.pengguna = 'CD-Kolla' //username
 global.footer = 'ZIM BOT INC'
 global.botnma = 'ZIM BOT INC' //bot name
 global.botname = 'ZIM BOT V4'
 global.ownernma = 'Drips' //owner name
 global.packname = 'Drips' //sticker package name
-global.author = 'By Drips Memes' //sticker author name
+global.author = 'By CD-Kolla' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '🔵' //sp
