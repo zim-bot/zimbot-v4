3110aecc06e9ea4e290dc218ae32f44c2c5bf711
DRIPS MEMES BLENNIE BLESSED TUNA <reinhardtuna@gmail.com>
2023-03-14 10:19:20 +0200

    new update

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const server = express()
+const fetch = require('node-fetch')
+const PORT = process.env.PORT || 3000
+server.set('json spaces', 2)
+server.get('/', async (req, res) => {
+   res.json({
+      online: true,
+      msg: `Server running with port ${PORT}`,
+      server: await (await fetch('http://ip-api.com/json')).json()
+   })
+})
+
+server.listen(PORT, () => console.log(`Zim-bot server running with port ${PORT}!`))
